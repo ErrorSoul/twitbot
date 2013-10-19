0a2e1b2467a3eb41a714717f67cd6a498c0149a5
Abu Sultanov <neurofag@gmail.com>
2013-10-19 19:15:57 +0400

    add thread for update tweets, not replays

--- twitbot.py
+++ twitbot.py
@@ -3,6 +3,7 @@
 from twython import Twython, TwythonError 
 from random import choice
 from datetime import datetime
+from threading import Thread
 from time import sleep
 from text import replays, night, afternoon, morning
 
@@ -20,28 +21,33 @@ text = [morning, afternoon, night]
 
 
 class TwitBot(object):
+    time = [(4, 59), (13, 59), (19, 59)]
+
     def __init__(self,c_key, c_secret,
                  o_token, o_token_secret):
         self.twitter = Twython(c_key, c_secret, o_token, o_token_secret)
         self.id = [391240789764112385]
         self.jd = [391091716893458433]
-        self.time = [(4, 59), (13, 59), (19, 59)]
+        
 
     def run_search(self, query, text):
         self.query = query 
         result =  self.twitter.search(q=query)
         if result:
            
-            s = [(int(c["id"]), c["user"]["screen_name"]) for c in result["statuses"]]
+            s = [(int(c["id"]), c["user"]["screen_name"])
+                 for c in result["statuses"] if
+                 c["user"]["screen_name"] != u"ghohol"]
+
             s = sorted(s)
             print s
-            for user in s:
-                self.update_status(text,user)
-            sleep(300)
-
+            ## for user in s:
+            ##     self.update_status(text,user)
+            ## sleep(300)
+            
     def update_status(self, text, user):
         q_id = self.id if self.query == u"хохол" else self.jd
-        if user[1] != u"ghohol" and user[0] > q_id[0]:
+        if  user[0] > q_id[0]:
             try:
                 self.twitter.update_status(status= u"@{0} {1}".format(
                                            user[1], choice(text)),
@@ -51,28 +57,43 @@ class TwitBot(object):
             except TwythonError as err:
                 print err
                 sleep(400)
-
+    
     def date_status(self, text):
         current_time = datetime.now()
+        print current_time, "fffffffffffffff"
         def part_of_day(a):
             return current_time.hour == a[0] and current_time.minute in range(a[1])
-        s = filter(part_of_day,self.time)
+        s = filter(part_of_day, self.time)
         if s:
+            print "tttttt"
             message =  choice(text[self.time.index(s[0])])
             try:
                 self.twitter.update_status(status= u"{0}".format(message))
-                sleep(360)
+                sleep(20)
             except TwythonError as err:
                 print err
                 sleep(280)
+
+class T_date(Thread):
+    def __init__(self, twitter):
+        Thread.__init__(self)
+        self.twitter= twitter
+    
+    def run(self):
+        while True:
+            self.twitter.date_status(text)
+    
 
 
 if __name__ == "__main__":
     twitter = TwitBot(CONSUMER_KEY,CONSUMER_SECRET,
                       OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+    t = T_date(twitter)
+    t.daemon = True
+    t.start()
     while True:
-        twitter.date_status(text)
+        #twitter.date_status(text)
         for c in g:
             twitter.run_search(c,replays)
-            sleep(480)
+            #sleep(480)
         sleep(500)

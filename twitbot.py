ae99d58c31647cc47c4f81a05171b720de2f44e4
Abu Sultanov <neurofag@gmail.com>
2013-10-20 17:32:57 +0400

    add new tweets on text.py and some refactoring

--- twitbot.py
+++ twitbot.py
@@ -11,13 +11,8 @@ CONSUMER_KEY       = "Dc8GNbgcOklJie3TV2V0A"
 CONSUMER_SECRET    = "QducleApTwunlqZmNM0AdhwlhWpoDJ44agk6UnPs"
 OAUTH_TOKEN        = "1964645443-7XZiMnTEN0fp5e2CvuyYrsM8YralkeCfya6y2Th"
 OAUTH_TOKEN_SECRET = "hcA1vYDtB5e3XVFHRzunNP5VBO0JBvxDGKeAIab3w"
-
-
-g = [u"хахол", u"хохол"]
-
-   
-
-text = [morning, afternoon, night]
+QUERYS = [u"хахол", u"хохол"]
+TEXT = [morning, afternoon, night]
 
 
 class TwitBot(object):
@@ -26,34 +21,45 @@ class TwitBot(object):
     def __init__(self,c_key, c_secret,
                  o_token, o_token_secret):
         self.twitter = Twython(c_key, c_secret, o_token, o_token_secret)
+
+        #last tweet id for query "xохол"
         self.id = [391240789764112385]
+
+        #last tweet id for query "xахол"
         self.jd = [391091716893458433]
-        self.time = [(4, 59), (13, 59), (20, 59)]
+
+        #list of time_of_days, when bot should update status
+        self.time = [(4, 59), (17, 59), (19, 59)]
         
 
     def run_search(self, query, text):
         self.query = query 
         result =  self.twitter.search(q=query)
         if result:
-           
-            s = [(int(c["id"]), c["user"]["screen_name"])
-                 for c in result["statuses"] if
-                 c["user"]["screen_name"] != u"ghohol"]
 
-            s = sorted(s)
-            print s
+            #users = [(tweet id, user screen_name)]
+            users = [(int(c["id"]), c["user"]["screen_name"])
+                     for c in result["statuses"] if
+                     c["user"]["screen_name"] != u"ghohol"]
+
+            #sorted tweets from older to younger
+            users = sorted(users)
+            print users
             print "#" * 20
             ## for user in s:
             ##     self.update_status(text,user)
-            ## sleep(300)
+            sleep(5)
             
     def update_status(self, text, user):
         q_id = self.id if self.query == u"хохол" else self.jd
+
+        #check replayed tweed
         if  user[0] > q_id[0]:
             try:
                 self.twitter.update_status(status= u"@{0} {1}".format(
                                            user[1], choice(text)),
                                            in_reply_to_status_id=user[0])
+                #save last tweet id 
                 q_id[0] = user[0]
                 sleep(480)
             except TwythonError as err:
@@ -68,13 +74,16 @@ class TwitBot(object):
     
     def date_status(self, text):
         current_time = datetime.now()
-        print current_time, "fffffffffffffff"
+        print "current time = {0}".format(str(current_time))
         
-        def part_of_day(a):
-            return current_time.hour == a[0] and current_time.minute in range(a[1])
-        s = filter(part_of_day, self.time)
-        if s:
-            print "tttttt"
+        def part_of_day(time_of_day):
+            """return True if current time equal time_of_day"""
+            return (current_time.hour == time_of_day[0] and
+                    current_time.minute in range(time_of_day[1]))
+
+        check_time = filter(part_of_day, self.time)
+        if check_time:
+            print "check_time"
             sleep(10)
             ## message =  choice(text[self.time.index(s[0])])
             ## try:
@@ -85,30 +94,31 @@ class TwitBot(object):
             ##     sleep(280)
 
 class T_date(Thread):
+    """Class for update status on time"""
     def __init__(self, twitter):
         Thread.__init__(self)
         self.twitter= twitter
     
     def run(self):
         while True:
-            self.twitter.date_status(text)
+            self.twitter.date_status(TEXT)
     
 
 
 if __name__ == "__main__":
     twitter = TwitBot(CONSUMER_KEY,CONSUMER_SECRET,
                       OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-    a = twitter.get_followers_ids()
+    a = twitter.my_reetwets()
     print a
-    for c in a:
-        print c
-        print "*" * 20 
+    ## for c in a:
+    ##     print c
+    ##     print "*" * 20 
     ## t = T_date(twitter)
     ## t.daemon = True
     ## t.start()
     ## while True:
-    ##     twitter.date_status(text)
-    ##     for c in g:
-    ##         twitter.run_search(c,replays)
+    ##     #twitter.date_status(text)
+    ##     for query in QUERYS:
+    ##         twitter.run_search(query,replays)
     ##         sleep(480)
     ##     sleep(5)

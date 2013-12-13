b43bad9a166f38468f02af6a30c9e1a12d0e6061
Abu Sultanov <neurofag@gmail.com>
2013-12-14 01:24:02 +0400

    experiment with thread

--- stream.py
+++ stream.py
@@ -1,5 +1,7 @@
+from threading import Thread 
 from twython import Twython
 from twython import TwythonStreamer
+import time 
 
 CONSUMER_KEY = "Dc8GNbgcOklJie3TV2V0A"
 CONSUMER_SECRET = "QducleApTwunlqZmNM0AdhwlhWpoDJ44agk6UnPs"
@@ -8,15 +10,43 @@ oauth_token='74446564-IasJQxyO7JzgnbRhyl5bamFQSzUwQDG4LB68KHjqZ'
 
 
 class MyStreamer(TwythonStreamer):
+    def x(self, gen):
+        self.generator = gen()
+
+    def terminator(self):
+        time.sleep(2)
+        c = 0 
+        while c < 100000:
+            print c
+            if c == 50000:
+                self.connected=False
+            c += 1
+        
     def on_success(self, data):
+        
+        
        
         if 'text' in data:
-            print data['text'].encode('utf-8')
+            print (data['text'].encode('utf-8'))
+        if not self.connected:
+            print "ssssssssssss"
 
     def on_error(self, status_code, data):
         print status_code
-stream = MyStreamer(CONSUMER_KEY, CONSUMER_SECRET, oauth_token, oauth_token_secret)
-#print stream.statuses.firehose()
 
-stream.user(replies=all)
+    def run(self):
+        self.user()
+        print "Hleb"
+        self.disconnect()
+stream = MyStreamer(CONSUMER_KEY, CONSUMER_SECRET, oauth_token, oauth_token_secret,timeout=300)
+def gena():
+    
+    while True:
+        x = yield a
+        print x 
+t = Thread(target=stream.user)
+t.start
+#stream.x(gena)
+#stream.generator.next()
+stream.user()
 
